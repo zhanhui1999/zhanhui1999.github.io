90123115b31a2f962a73f6bcee17cd23d50ee1c9
zhanhui1999 <zhanhui@zju.edu.cn>
2022-07-06 16:45:45 +0800

    test

--- mysql_gs/privileges.sql
+++
@@ -1,4 +0,0 @@
-use mysql;
-create user if not exists graphscope identified by 'graphscope';
-grant all on graphscope.* to graphscope@'%' identified by 'graphscope' with grant option;
-flush privileges;
